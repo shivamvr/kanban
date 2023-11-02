39717edd96c277fb83c47d54c0c07bfa4dc3e22e
shivamvr <shivamweb2@gmail.com>
2023-11-02 17:29:24 +0530

    minor

--- db.js
+++ db.js
@@ -4,8 +4,8 @@ const mongoose = require('mongoose')
 
 require('dotenv')
 
-// const connection = mongoose.connect('mongodb+srv://shivam:golmaal5@cluster0.10lgcrz.mongodb.net/kanban')
-const connection = mongoose.connect(process.env.MONGO_URL)
+const connection = mongoose.connect('mongodb+srv://shivam:golmaal5@cluster0.10lgcrz.mongodb.net/kanban')
+// const connection = mongoose.connect(process.env.MONGO_URL)
 
 
 module.exports = {connection}
